8a15eb35b05b79f7be24cf7fc717065d6fe89d94
Jonathan Bridges <jonathan.bridges@mercy.net>
2015-08-09 20:35:58 -0500

    Create MapDrawer service

---
+++ Assets/Scripts/Services/MapDrawer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapDrawer : MonoBehaviour {
+
+	public int width;
+	public int height;
+	[Range(0,100)] 
+	public int randomFillPercent;
+
+	private MapGenerator generator;
+	private int[,] map;
+
+	void Start() {
+		generator = new MapGenerator(width, height, randomFillPercent);
+		map = generator.GenerateMap();
+	}
+	
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			map = generator.GenerateMap();
+		}
+	}
+
+	void OnDrawGizmos() {
+		if (map != null) {
+			for (int x = 0; x < width; x ++) {
+				for (int y = 0; y < height; y ++) {
+					Gizmos.color = (map[x,y] == 1)?Color.black:Color.white;
+					Vector3 pos = new Vector3(-width/2 + x + .5f,-height/2 + y+.5f, 0);
+					Gizmos.DrawCube(pos,Vector3.one);
+				}
+			}
+		}
+	}
+}
--- Assets/Scripts/Services/MapGenerator.cs
+++ Assets/Scripts/Services/MapGenerator.cs
@@ -3,41 +3,32 @@ using System.Collections;
 
 public class MapGenerator : MonoBehaviour {
 
-	public int width;
-	public int height;
+	private int width;
+	private int height;
+	private int randomFillPercent;
 	
-	public string seed;
-	public bool useRandomSeed;
-	
-	[Range(0,100)]
-	public int randomFillPercent;
-	
-	int[,] map;
-	
-	void Start() {
-		GenerateMap();
+	private int[,] map;
+
+	public MapGenerator(int width, int height, int randomFillPercent) {
+		this.width = width;
+		this.height = height;
+		this.randomFillPercent = randomFillPercent;
 	}
 	
-	void Update() {
-		if (Input.GetMouseButtonDown(0)) {
-			GenerateMap();
-		}
-	}
-	
-	void GenerateMap() {
+	public int[,] GenerateMap() {
 		map = new int[width,height];
 		RandomFillMap();
 		
 		for (int i = 0; i < 5; i ++) {
 			SmoothMap();
 		}
+
+		return map;
 	}
 	
 	
 	void RandomFillMap() {
-		if (useRandomSeed) {
-			seed = Time.time.ToString();
-		}
+		string seed = Time.time.ToString();
 		
 		System.Random pseudoRandom = new System.Random(seed.GetHashCode());
 		
@@ -83,18 +74,5 @@ public class MapGenerator : MonoBehaviour {
 		}
 		
 		return wallCount;
-	}
-	
-	
-	void OnDrawGizmos() {
-		if (map != null) {
-			for (int x = 0; x < width; x ++) {
-				for (int y = 0; y < height; y ++) {
-					Gizmos.color = (map[x,y] == 1)?Color.black:Color.white;
-					Vector3 pos = new Vector3(-width/2 + x + .5f,-height/2 + y+.5f, 0);
-					Gizmos.DrawCube(pos,Vector3.one);
-				}
-			}
-		}
 	}
 }
